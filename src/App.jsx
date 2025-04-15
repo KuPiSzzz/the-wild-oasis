032e388ca286bb65afc4fb76d5b05a2f77ffbd5f
KuPiSzzz <kupisz2013@gmail.com>
2025-04-15 23:20:10 +0200

    Adding styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading as="h1">The Wild Oasis</Heading>
+        <Heading as="h2">Check in and out</Heading>
+        <Button>Chceck in</Button>
+        <Button>Chceck out</Button>
+
+        <Heading as="h3">Form</Heading>
+        <Input type="number" placeholder="Number of guests" />
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
