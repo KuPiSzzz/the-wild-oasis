ac079b038d3a92f41a9d37a48129c905311fbdcb
KuPiSzzz <kupisz2013@gmail.com>
2025-04-24 23:56:45 +0200

    Adding Logic

---
+++ src/features/cabins/CabinTableOperations.jsx
@@ -0,0 +1,12 @@
+import TableOperions from "../../ui/TableOperations";
+import Filter from "../../ui/Filter";
+
+function CabinTableOperations() {
+  return (
+    <TableOperions>
+      <Filter />
+    </TableOperions>
+  );
+}
+
+export default CabinTableOperations;
