dac37a9853b7f2f16f27e5e8dba9944e0aac0c82
KuPiSzzz <kupisz2013@gmail.com>
2025-05-09 13:38:59 +0200

    Adding login-in and out functionality

---
+++ src/features/authentication/Logout.jsx
@@ -0,0 +1,15 @@
+import { HiArrowRightOnRectangle } from "react-icons/hi2";
+import ButtonIcon from "../../ui/ButtonIcon";
+import { useLogout } from "./useLogout";
+import SpinnerMini from "../../ui/SpinnerMini";
+
+function Logout() {
+  const { logout, isPending: isLogingOut } = useLogout();
+  return (
+    <ButtonIcon disabled={isLogingOut} onClick={logout}>
+      {!isLogingOut ? <HiArrowRightOnRectangle /> : <SpinnerMini />}
+    </ButtonIcon>
+  );
+}
+
+export default Logout;
